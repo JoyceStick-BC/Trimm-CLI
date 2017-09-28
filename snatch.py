21a6e5ac6ba4d579297004194dcc407c9ce418aa
CoolDude53 <pickle.plaza@gmail.com>
2017-09-28 17:19:27 -0400

    Added some json things.

--- snatch.py
+++ snatch.py
@@ -2,18 +2,40 @@ import click
 import requests
 import io
 import zipfile
+import os
+import json
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option('--name', prompt='Please enter your name/package', help='Your package name in format name/package.')
-@click.option('--path', help='Absolute path to install packages. Defaults to current dir.')
+@click.option('--path', help='Absolute path to install packages. Defaults to currentDir/Assets/vendor.')
 @click.option('--version', help='Version number for package.')
-def pull(name, path, version):
-    url = "http://snatch.joycestick.com/api/" + name
+def install(name, path, version):
+    url = "http://fallingkingdom.net/" + name + ".zip"  # "http://snatch.joycestick.com/api/" + name + "/download"
     if version is not None:
         url += "/" + version
 
     download(url, path)
+
+
+@cli.command()
+@click.option('--path', help='Absolute path to locate info.json. Defaults to currentDir/Assets/vendor.')
+def pull(path):
+    # check json for new assets and/or new versions of assets
+    # download(url, path)
+
+    if path is None:
+        path = os.getcwd() + os.pathsep + "Assets" + os.pathsep + "vendor" + os.pathsep
+
+    with open(path + "info.json") as data_file:
+        data = json.load(data_file)
+
+    print(data["assets"][0]["version"])
 
 
 # installs unzipped package to the given directory
@@ -25,6 +47,7 @@ def download(url, path):
         print("Bad response for url: %s" % url)
         return
 
+    # make sure we have a zip file
     byte_file = io.BytesIO(returned_request.content)
     if not zipfile.is_zipfile(byte_file):
         print("Returned file is not a zip at url: %s" % url)
@@ -33,14 +56,23 @@ def download(url, path):
     # create a zipfile object
     zip_file = zipfile.ZipFile(byte_file)
 
+    # set extract path
+    if path is None:
+        path = os.path.join(os.getcwd(), "Assets")
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path = os.path.join(path, "vendor")
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path += os.sep
+
+    print(path)
+
     # extract zip
-    if path is not None:
-        zip_file.extractall(path)
-    else:
-        zip_file.extractall()
+    zip_file.extractall(path)
 
     print("Success!")
 
 
 if __name__ == '__main__':
-    pull()
+    cli()
