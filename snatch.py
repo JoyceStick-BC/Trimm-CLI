733c707f2857fc7051d642ba4246a62137209855
CoolDude53 <pickle.plaza@gmail.com>
2017-10-10 15:49:57 -0400

    Finalized installing. Now handles all sorts of bundles, including packages inside of packages.

--- snatch.py
+++ snatch.py
@@ -17,8 +17,8 @@ def cli():
 @click.option('--path', help='Absolute path to install packages. Defaults to currentDir/Assets/vendor.')
 @click.option('--version', help='Version number for package.')
 def install(bundlename, path, version):
-    # url = "http://snatch-it.org/" + bundlename + "/download"
-    url = "http://fallingkingdom.net/" + bundlename + ".zip"
+    url = "http://snatch-it.org/" + bundlename + "/download"
+    # url = "http://fallingkingdom.net/" + bundlename + ".zip"
     if version is not None:
         url += "/" + version
 
@@ -89,36 +89,14 @@ def download(bundlename, url, path):
     zip_file.extractall(downloading_path)
 
     info_jsons = []
-    # now let's unzip all the inner zips
-    for filename in os.listdir(downloading_path):
-        new_path = os.path.join(downloading_path, filename)
-        if os.path.isdir(new_path):
-            for unzipped_filename in os.listdir(new_path):
-                inner_dir_path = os.path.join(new_path, unzipped_filename)
-                if unzipped_filename == "info.json":
-                    inner_data_file = open(inner_dir_path, 'r')
-                    inner_info_json = json.load(inner_data_file)
-                    info_jsons.append(inner_info_json)
-                    if inner_info_json["type"] == "asset":
-                        inner_asset_path = os.path.join(new_path, inner_info_json["name"] + ".zip") # need to change name to bundlename after testing
-                        print("Unzipping " + inner_info_json["bundlename"] + "!")
-                        inner_zip_file = zipfile.ZipFile(inner_asset_path)
-                        inner_zip_file.extractall(path)
-                    elif inner_info_json["type"] == "package":
-                        for unzipped_package_filename in os.listdir(inner_dir_path):
-                            inner_package_file = os.path.join(new_path, unzipped_package_filename)
-                            if zipfile.is_zipfile(inner_package_file):
-                                print("Unzipping " + unzipped_package_filename[:-4] + " from " + inner_info_json["bundlename"] + "!")
-                                inner_zip_file = zipfile.ZipFile(inner_package_file)
-                                inner_zip_file.extractall(inner_dir_path)
-                                # need to handle the contents of the package here
+    drill(downloading_path, path, info_jsons)
 
-        # let's go over all the jsons and add them to our snatch.json
-        for info_json in info_jsons:
-            if info_json["type"] == "asset":
-                snatch_assets[info_json["bundlename"]] = info_json["version"]
-            elif info_json["type"] == "package":
-                snatch_packages[info_json["bundlename"]] = info_json["version"]
+    # let's go over all the jsons and add them to our snatch.json
+    for info_json in info_jsons:
+        if info_json["type"] == "asset":
+            snatch_assets[info_json["bundlename"]] = info_json["version"]
+        elif info_json["type"] == "package":
+            snatch_packages[info_json["bundlename"]] = info_json["version"]
 
     # delete the downloading folder and output.bin
     os.remove("output.bin")
@@ -129,6 +107,51 @@ def download(bundlename, url, path):
         json.dump(snatch_json, out_file, indent=4, sort_keys=True)
 
     print("Successfully installed " + bundlename + "!")
+
+
+def drill(bundle_path, vendor_path, info_jsons):
+    # let's get all the files in the downloading path
+    for filename in os.listdir(bundle_path):
+        new_path = os.path.join(bundle_path, filename)
+        # if we find a directory
+        if os.path.isdir(new_path):
+            # let's look for zips in this dir by identifying any info.jsons
+            for unzipped_filename in os.listdir(new_path):
+                inner_dir_path = os.path.join(new_path, unzipped_filename)
+                # if we find an info.json, let's unzip it's associated zip
+                if unzipped_filename == "info.json":
+                    # add the info to our list of info jsons
+                    inner_data_file = open(inner_dir_path, 'r')
+                    inner_info_json = json.load(inner_data_file)
+                    info_jsons.append(inner_info_json)
+
+                    # if this bundle is an asset
+                    if inner_info_json["type"] == "asset":
+                        # let's extract the asset zip to the vendor path
+                        inner_asset_path = os.path.join(new_path, inner_info_json["bundlename"] + ".zip")  # need to change bundlename to name for fk testing
+                        print("Unzipping " + inner_info_json["bundlename"] + "!")
+                        inner_zip_file = zipfile.ZipFile(inner_asset_path)
+                        inner_zip_file.extractall(vendor_path)
+
+                        # after extracting zip, let's delete
+                        os.remove(inner_asset_path)
+
+                    # if this bundle is a package
+                    elif inner_info_json["type"] == "package":
+                        # let's extract all the bundles of this package
+                        for unzipped_package_filename in os.listdir(new_path):
+                            inner_package_file = os.path.join(new_path, unzipped_package_filename)
+
+                            # let's extract the inner bundle zips to this dir
+                            if zipfile.is_zipfile(inner_package_file):
+                                inner_zip_file = zipfile.ZipFile(inner_package_file)
+                                inner_zip_file.extractall(new_path)
+
+                                # after extracting zip, let's delete
+                                os.remove(inner_package_file)
+
+                        # now let's drill again to handle the bundles of the package
+                        drill(new_path, vendor_path, info_jsons)
 
 
 if __name__ == '__main__':
